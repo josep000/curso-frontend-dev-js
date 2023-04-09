55d0debfb78b2b9ef7c4819a7ea308b36674ecdf
Jose Pinto <josep8686@gmail.com>
2023-04-09 09:43:48 -0300

    cambios listos en menu mobile

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+const burgerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+burgerMenu.addEventListener('click', toggleMobileMenu);
+
+
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
